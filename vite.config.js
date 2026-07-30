92a6539c916163b5972fc0de2e3c3bfe5bebb0ee
hormigonia <hormixia@gmail.com>
2026-07-30 12:43:44 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tecnico: resolve(__dirname, 'manual_tecnico.html'),
+        usuario: resolve(__dirname, 'manual_usuario.html'),
+        principiante: resolve(__dirname, 'manual_principiante.html')
+      }
+    }
   }
 });
